50576483ed1de153eefc5832f07fdd8bfb26f917
Anne-Hon-Heemus Nrugger <matt+nrug@blltd.eu>
2013-10-24 02:04:11 +0100

    Initial commit: Rails, welcome controller, testing with minitest spec and wrong

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nrug::Application.config.secret_key_base = 'b78f5532ab8714e9f12f6ad8cd8c79c827612ceacfbffb671012fb3eb047847ee267fb1469f0dce15cd66caffda383c0b19ce34eb2cd5ae62ef8e63b9bffa188'
---
+++ test/controllers/welcome_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class WelcomeControllerTest < ActionController::TestCase
+  it "should accept Wrong as the one true testing framework" do
+    assert{ "everything I do".split(/[I do it for you]+/).length == 4 }
+  end
+end
