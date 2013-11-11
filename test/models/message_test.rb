a08a76fde0e8bf554ef76ba1030763e720a5e081
Jo Lovesey <lenores_kitty@hotmail.com>
2013-11-11 08:45:32 +0000

    Failing test - pending does not skip mocks in the before

--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -2,12 +2,12 @@ require 'test_helper'
 
 describe Message do
   before do
-    Message.expects(:create!).returns Message.new
-    Server.expects(:current).returns Server.new()
+    # Message.expects(:create!).returns Message.new
+    # Server.expects(:current).returns Server.new()
     stub_request(:any, 'example.com').to_return(:body => '')
   end
 
   it "should transmit a message to a given url" do
-    pending
+    skip
   end
 end
