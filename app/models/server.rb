f0c6b555ffdd789643036aa8c2320704306909bc
Jo Lovesey <lenores_kitty@hotmail.com>
2013-11-19 05:42:37 +0000

    straggler

--- app/models/server.rb
+++ app/models/server.rb
@@ -17,10 +17,6 @@ class Server < ActiveRecord::Base
     def make_main(url)
       Server.find_or_create_by(:hostname => url).update_attribute :current_role, 'server'
     end
-
-    def make_badger
-
-    end
   end
 
   has_one :board
